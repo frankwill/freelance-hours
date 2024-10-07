e2dc39007cac87c3304a78745fb2b2f7178af1e2
Frank William <frank.will999@gmail.com>
2024-10-07 10:48:00 -0300

    criando controller

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', WelcomeController::class);
 
 Route::get('outra-rota', function () {
     echo "oi";
